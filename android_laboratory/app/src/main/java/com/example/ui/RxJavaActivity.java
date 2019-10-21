01816353c7e7fa6884de29f19209cb09e5e0278b
anymyna <anymyna@126.com>
2019-10-22 00:13:48 +0800

    add rxjava

--- android_laboratory/app/src/main/java/com/example/ui/RxJavaActivity.java
+++ android_laboratory/app/src/main/java/com/example/ui/RxJavaActivity.java
@@ -2,14 +2,132 @@ package com.example.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.orhanobut.logger.Logger;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 
 public class RxJavaActivity extends AppCompatActivity {
+    private String tag = "RxJavaActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_rxjava);
+
+        Logger.e("RxJavaActivity onCreate ");
+
+        //fun1();
+        //fun2();
+//        fun3();
+        fun4();
+
+
     }
+
+
+    private void fun1()
+    {
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.d(tag, "Item: " + s);
+            }
+
+            @Override
+            public void onCompleted() {
+                Log.d(tag, "Completed!");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(tag, "Error!");
+            }
+        };
+
+
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("Hi");
+                subscriber.onNext("Aloha");
+                subscriber.onCompleted();
+            }
+        });
+
+
+        observable.subscribe(observer);
+    }
+
+
+    private void fun2()
+    {
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.d(tag, "Item: " + s);
+            }
+
+            @Override
+            public void onCompleted() {
+                Log.d(tag, "Completed!");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(tag, "Error!");
+            }
+
+            @Override
+            public void onStart() {
+                super.onStart();
+                Log.d(tag, "onStart!");
+            }
+        };
+
+        //Observable observable = Observable.just("Hello", "Hi", "Aloha");
+
+        String[] words = {"Hello", "Hi", "Aloha"};
+        Observable observable = Observable.from(words);
+
+        observable.subscribe(subscriber);
+    }
+
+
+    private void fun3()
+    {
+        String[] words = {"Hello", "Hi", "Aloha"};
+        Observable.from(words)
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String name) {
+                        Log.d(tag, name);
+                    }
+                });
+    }
+
+
+    // 线程控制
+    private void fun4()
+    {
+        Observable.just(1, 2, 3, 4)
+                .subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程
+                .observeOn(AndroidSchedulers.mainThread()) // 指定 Subscriber 的回调发生在主线程
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer number) {
+                        Log.d(tag, "number:" + number);
+                    }
+                });
+    }
+
 
 }
